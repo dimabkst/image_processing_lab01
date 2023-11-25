b9f7812956391d9cefc3b67c75847e7cfeb072fe
dimabkst <dimabkst@gmail.com>
2023-11-25 18:22:18 +0200

    mirorred image

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from traceback import print_exc
 from PIL import Image
 from utils import convertToListImage, convertToPillowImage, saveImage
-from services import getMean, getVariance, getStandardDeviation, getMSE, getRMSE, getPSNR, addGaussianAdditiveNoise
+from services import getMean, getVariance, getStandardDeviation, getMSE, getRMSE, getPSNR, addGaussianAdditiveNoise, mirrorImage
 
 if __name__ == "__main__":
     try:
@@ -15,12 +15,18 @@ if __name__ == "__main__":
             standard_deviation = getStandardDeviation(image, mean, variance)
 
             print(f'Mean={mean}, variance={variance}, standard deviation={standard_deviation}.')
-            
+
             std_dev_coef1 = 0.2
 
             noisy_image1 = addGaussianAdditiveNoise(image, std_dev_coef1)
 
             saveImage(convertToPillowImage(noisy_image1), f'./assets/cameraman_noisy_sigma={std_dev_coef1}.tif')
+
+            filter_kernel_size = 250
+
+            mirroredImage = mirrorImage(image, (filter_kernel_size, filter_kernel_size))
+
+            saveImage(convertToPillowImage(mirroredImage), f'./assets/cameraman_mirrored_size={filter_kernel_size}.tif')
     except Exception as e:
         print('Error occured:')
         print_exc()
--- services.py
+++ services.py
@@ -1,5 +1,5 @@
-from typing import List, Union
-from cmath import log10
+from typing import List, Tuple, Union
+from math import log10, floor 
 from constants import MAX_NUMBER_OF_INTENSITY_LEVELS
 from utils import generateGaussianNoise, convertToProperImage
 
@@ -48,7 +48,7 @@ def getPSNR(image1: List[List[int]], image2: List[List[int]], MSE: Union[float, 
 
     RMSE = RMSE if RMSE is not None else getRMSE(image1, image2, MSE)
 
-    PSNR = (20 * log10((L - 1) / RMSE)).real
+    PSNR = (20 * log10((L - 1) / RMSE))
 
     return PSNR
 
@@ -65,3 +65,48 @@ def addGaussianAdditiveNoise(image: List[List[int]], std_dev_coef: float) -> Lis
     noisy_image = [[image[i][j] + noise[i][j] - mean for j in range(M)] for i in range(N)]
 
     return convertToProperImage(noisy_image)
+
+def mirrorImage(image: List[List[int]], filterKernelSizes: Tuple[int, int]) -> List[List[int]]:
+    N = len(image)
+    M = len(image[0])
+    
+    extensionSizes = tuple(floor(size / 2) for size in filterKernelSizes)
+
+    mirroredImage = []
+
+    for i in range(-extensionSizes[0], N + extensionSizes[0]):
+        mirroredImage.append([])
+
+        if i < 0:
+            ii = -i
+        elif i >= N:
+            ii = N - 1 - (i - N + 1)
+        else:
+            ii = i
+
+        for j in range(-extensionSizes[1], M + extensionSizes[1]):
+            if j < 0:
+                jj = -j
+            elif j >= M:
+                jj = M - 1 - (j - M + 1)
+            else:
+                jj = j
+
+            # if i < 0 and j < 0:
+            #     pixel = image[-i][-j]
+            # elif i < 0:
+            #     pixel = image[-i][-j]
+            # elif j < 0:
+            #     pixel = image[i][-j]
+            # elif i >= N and j >= M:
+            #     pixel = image[N - 1 - (i - N + 1)][M - 1 - (j - M + 1)]
+            # elif i >= N:
+            #     pixel = image[N - 1 - (i - N + 1)][j]
+            # elif i >= M:
+            #     pixel = image[i][M - 1 - (j - M + 1)]
+            # else:
+            #     pixel = image[i][j]
+
+            mirroredImage[-1].append(image[ii][jj])
+
+    return mirroredImage
